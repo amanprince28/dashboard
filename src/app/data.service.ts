72d91b8ec0c378725d61dd106318f6e7b1028fc4
Aman <amanprince28@gmail.com>
2024-10-15 21:04:54 +0800

    Added Login, Listing, details

---
+++ src/app/data.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SignalService } from './signal.service'; // Hypothetical circular dependency
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private apiUrl = 'http://localhost:3000';
+  private customer = this.apiUrl + '/customer';
+
+  constructor(private http: HttpClient, private signalService: SignalService) {} // Circular dependency
+
+  getCustomer(payload: any): Observable<any> {
+    const params = new HttpParams()
+    .set('page', payload.page.toString())
+    .set('limit', payload.limit.toString());
+    console.log('Fetching data -----', params); 
+    return this.http.get<any[]>(this.customer, { params });
+  }
+
+  getCustomerById(id: string): Observable<any> {
+    return this.http.get<any[]>(`${this.customer}/${id}`);
+  }
+
+  getCountry(countryId: string | null, name: string | null): Observable<any> {
+    let url = this.apiUrl + '/country';
+    const params = [];
+
+    if (countryId) {
+      params.push(`countryId=${countryId}`);
+    }
+
+    if (name) {
+      params.push(`name=${name}`);
+    }
+
+    if (params.length > 0) {
+      url += '?' + params.join('&');
+    }
+
+    return this.http.get<any[]>(url);
+  }
+
+  addCustomer(customer: any): Observable<any> {
+    const url = this.apiUrl + '/customer';
+    return this.http.post<any>(url, customer);
+  }
+
+  updateCustomer(customer: any): Observable<any> {
+    const url = this.apiUrl + '/customer';
+    return this.http.post<any>(url + '/:id', customer);
+  }
+}
---
+++ src/app/listing/listing.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit, signal, ViewChild } from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+
+import {MatButtonModule} from '@angular/material/button';
+import { Router } from '@angular/router';
+import { SignalService } from '../signal.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from '../data.service';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+
+
+@Component({
+  selector: 'app-listing',
+  standalone: true,
+  imports: [
+    MatTableModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    HttpClientModule, // Ensure HttpClientModule is imported here
+    MatPaginatorModule
+  ],
+  providers: [DataService], // Ensure DataService is provided here
+  templateUrl: './listing.component.html',
+  styleUrls: ['./listing.component.css']
+})
+export class ListingComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'ic', 'passport'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  dataSource = new MatTableDataSource<any>([]);
+  searchForm = new FormGroup({
+    search: new FormControl(''),
+  });
+  signalData = signal({});
+  search = new FormControl();
+
+  constructor(private router: Router, private signalService: SignalService, private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.paginator.page.subscribe(() => {
+      this.fetchData(this.paginator.pageIndex, this.paginator.pageSize);
+    });
+  }
+
+  fetchData(page: number = 0, limit: number = 5): void {
+    const payload = { page, limit};
+    this.dataService.getCustomer(payload).subscribe((response: any) => {
+      console.log(response);
+      this.dataSource.data = response.data;
+    });
+  }
+
+  onRowClick(row: any): void {
+    if (!row.id) {
+      return;
+    }
+    this.dataService.getCustomerById(row.id).subscribe((response: any) => {
+      this.signalService.triggerAction(response);
+      this.router.navigate(['/details', row.id]);
+    });
+  }
+
+  // For filtering the table
+  filterTable(): void {
+    if (this.searchForm.get('search')?.value) {
+      console.log(this.searchForm.get('search')?.value);
+      this.dataSource.filter = this.searchForm.get('search')?.value as string;
+      return;
+    }
+    this.dataSource.filter = '';
+  }
+
+  onAddClick(): void {
+    this.router.navigate(['/details']);
+  }
+}
